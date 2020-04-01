f79205a0c493e055c44c84f0f37a44e98ec85d62
Potoyan <potoyan@potoyan-mbp2017.lan>
2020-04-01 13:50:33 -0500

    freeE

--- 05_Ising_MC/codes/.ipynb_checkpoints/optim_ising2d-checkpoint.jl
+++ 05_Ising_MC/codes/.ipynb_checkpoints/optim_ising2d-checkpoint.jl
@@ -3,14 +3,15 @@ using Statistics
 using PyPlot
 
 ######## Main Code ###########
-function ising2d_mcmc(N,J,B,T; n_steps = 10^7, out_freq = 5000)
+
+function ising2d_mcmc(N::Int8, J::Float64, B::Float64, T::Float64; n_steps::Int64 = 10^7, out_freq::Int64 = 10^4)
 
     #Initialize
-    spins    = ones((N,N))        #rand([-1,1], (N,N))
+    spins    = ones(Int8,(N,N))  #rand([-1,1], (N,N))
 
     # Empty array/matrix for recording
-    confs    = []
-    M, E     = [], []
+    confs    = Int8[]
+    M, E     = Float64[], Float64[]
 
     for step in 1:n_steps
 
@@ -42,17 +43,18 @@ function ising2d_mcmc(N,J,B,T; n_steps = 10^7, out_freq = 5000)
 
 end
 
+
 ######## Thermo output ###########
 
 # Compute M
-function getM(spins) 
+function getM(spins::Array{Int8,2})
     
     return mean(spins) 
     
 end
 
 # Compute E
-function getE(spins,N,J,B) 
+function getE(spins::Array{Int8,2}, N::Int64, J::Float64, B::Float64)
     
     E = 0
     
