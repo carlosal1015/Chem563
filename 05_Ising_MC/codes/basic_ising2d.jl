27b6ca0706cdf06014327bb6af0849edde47f02c
Potoyan <potoyan@potoyan-mbp2017.lan>
2020-04-13 13:36:17 -0500

    LJ-MC2

--- 05_Ising_MC/codes/basic_ising2d.jl
+++ 05_Ising_MC/codes/basic_ising2d.jl
@@ -23,18 +23,18 @@ function ising2d_mcmc(N,J,B,T; n_steps = 10^7, out_freq = 5000)
 
         #Metropolis condition
         if dE <= 0  ||  exp(-dE/T) > rand()
-            
+
             spins[i,j] *= -1 # flip spin
-            
+
         end
 
         #Store the spin configuration
         if step % out_freq ==0
-            
+
             push!(confs,  copy(spins))
             push!(M,      getM(spins))
             push!(E,      getE(spins,N,J,B))
-                
+
         end
 
     end
@@ -45,33 +45,31 @@ end
 ######## Thermo output ###########
 
 # Compute M
-function getM(spins) 
-    
-    return mean(spins) 
-    
+function getM(spins)
+
+    return mean(spins)
+
 end
 
 # Compute E
-function getE(spins,N,J,B) 
-    
+function getE(spins,N,J,B)
+
     E = 0
-    
+
     for i in 1:N
         for j in 1:N
-            
+
             z = spins[mod1(i+1,N), j] + spins[mod1(i-1,N), j] + spins[i, mod1(j+1,N)] + spins[i, mod1(j-1,N)]
-            E += -J*z*spins[i,j]/4 
-            
+            E += -J*z*spins[i,j]/4
+
         end
     end
-    
-    return E-B*sum(spins) 
-    
+
+    return E-B*sum(spins)
+
 end
 
 
 ##### Print info #####
 
 println("Julia MC loaded\n 2 functions are availible:\n ising2d_mcmc(N,J,B,T, n_steps, out_freq) and getE(spins,N,J,B)\n Default number of steps is 10^7 with output frequency 5000.")
-
-
